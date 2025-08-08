38976d33b7ad232b7033ee127a93bcedc00708ad
Basti <biervertrieb@googlemail.com>
2025-08-08 16:19:19 +0200

    added CORS handling

---
+++ src/cors.php
@@ -0,0 +1,12 @@
+<?php
+
+use Slim\App;
+
+// Allow CORS for local dev
+$app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', 'http://localhost:5173')
+        ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
