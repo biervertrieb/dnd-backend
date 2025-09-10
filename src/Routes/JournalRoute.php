581cac59f415a372edf4279761d76db8280b0cf8
Basti <biervertrieb@googlemail.com>
2025-09-10 15:34:05 +0200

    declare as a function so the service dependency can be mocked for testing

--- src/Routes/JournalRoute.php
+++ src/Routes/JournalRoute.php
@@ -6,50 +6,53 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 
-$svc = JournalService::getInstance();
-
-$app->group('/journal', function ($group) use ($svc) {
-    $group->get('', function (Request $request, Response $response) use ($svc) {
-        $data = $svc->getEntries();
-        $response->getBody()->write(json_encode($data));
-        return $response->withHeader('Content-Type', 'application/json');
-    });
-
-    $group->post('', function (Request $request, Response $response) use ($svc) {
-        $input = $request->getParsedBody();
-        try {
-            $entry = $svc->addEntry($input['title'] ?? null, $input['body'] ?? null, $input['day'] ?? null);
-            $response->getBody()->write(json_encode(['status' => 'ok', 'entry' => $entry]));
+function registerJournalRoutes(App $app, JournalService $svc): void
+{
+    $app->group('/journal', function ($group) use ($svc) {
+        $group->get('', function (Request $request, Response $response) use ($svc) {
+            $data = $svc->getEntries();
+            $response->getBody()->write(json_encode($data));
             return $response->withHeader('Content-Type', 'application/json');
-        } catch (\RuntimeException $e) {
-            $response->getBody()->write(json_encode(['status' => 'error', 'message' => $e->getMessage()]));
-            return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
-        }
-    });
+        });
 
-    $group->put('/{id}', function (Request $request, Response $response, array $args) use ($svc) {
-        $id = $args['id'];
-        $input = $request->getParsedBody();
-        try {
-            $updated = $svc->updateEntry($id, $input['title'] ?? null, $input['body'] ?? null, $input['day'] ?? null);
-            $response->getBody()->write(json_encode(['status' => 'ok', 'entry' => $updated]));
-            return $response->withHeader('Content-Type', 'application/json');
-        } catch (\RuntimeException $e) {
-            $response->getBody()->write(json_encode(['status' => 'error', 'message' => $e->getMessage()]));
-            return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
-        }
-    });
+        $group->post('', function (Request $request, Response $response) use ($svc) {
+            $input = $request->getParsedBody();
+            try {
+                $entry = $svc->addEntry($input['title'] ?? null, $input['body'] ?? null, $input['day'] ?? null);
+                $response->getBody()->write(json_encode(['status' => 'ok', 'entry' => $entry]));
+                return $response->withHeader('Content-Type', 'application/json');
+            } catch (\RuntimeException $e) {
+                $response->getBody()->write(json_encode(['status' => 'error', 'message' => $e->getMessage()]));
+                return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
+            }
+        });
 
-    $group->delete('/{id}', function (Request $request, Response $response, array $args) use ($svc) {
-        $id = $args['id'];
-        $input = $request->getParsedBody();
-        try {
-            $updated = $svc->deleteEntry($id);
-            $response->getBody()->write(json_encode(['status' => 'ok', 'entry' => $updated]));
-            return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
-        } catch (\RuntimeException $e) {
-            $response->getBody()->write(json_encode(['status' => 'error', 'message' => $e->getMessage()]));
-            return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
-        }
-    });
-})->add(new JWTAuthMiddleware());
+        $group->put('/{id}', function (Request $request, Response $response, array $args) use ($svc) {
+            $id = $args['id'];
+            $input = $request->getParsedBody();
+            try {
+                $updated = $svc->updateEntry($id, $input['title'] ?? null, $input['body'] ?? null, $input['day'] ?? null);
+                $response->getBody()->write(json_encode(['status' => 'ok', 'entry' => $updated]));
+                return $response->withHeader('Content-Type', 'application/json');
+            } catch (\RuntimeException $e) {
+                $response->getBody()->write(json_encode(['status' => 'error', 'message' => $e->getMessage()]));
+                return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
+            }
+        });
+
+        $group->delete('/{id}', function (Request $request, Response $response, array $args) use ($svc) {
+            $id = $args['id'];
+            $input = $request->getParsedBody();
+            try {
+                $updated = $svc->deleteEntry($id);
+                $response->getBody()->write(json_encode(['status' => 'ok', 'entry' => $updated]));
+                return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
+            } catch (\RuntimeException $e) {
+                $response->getBody()->write(json_encode(['status' => 'error', 'message' => $e->getMessage()]));
+                return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
+            }
+        });
+    })->add(new JWTAuthMiddleware());
+}
+
+registerJournalRoutes($app, JournalService::getInstance());
