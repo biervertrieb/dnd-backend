945a1a63abec41bc3b9fea58ec7c920130b454b4
Basti <biervertrieb@googlemail.com>
2025-09-10 15:00:13 +0200

    use instance provider

--- src/Routes/JournalRoute.php
+++ src/Routes/JournalRoute.php
@@ -6,7 +6,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 
-$svc = new JournalService();
+$svc = JournalService::getInstance();
 
 $app->group('/journal', function ($group) use ($svc) {
     $group->get('', function (Request $request, Response $response) use ($svc) {
