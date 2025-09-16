a8a39396890ff7074f5ff7aa58d217fbcaaa8c08
Basti <biervertrieb@googlemail.com>
2025-09-16 21:47:27 +0200

    allow credentials

--- src/cors.php
+++ src/cors.php
@@ -8,6 +8,7 @@ $app->add(function ($request, $handler) {
         $response = new \Slim\Psr7\Response();
         return $response
             ->withHeader('Access-Control-Allow-Origin', 'http://localhost:5173')
+            ->withHeader('Access-Control-Allow-Credentials', 'true')
             ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->withStatus(204);
@@ -15,6 +16,7 @@ $app->add(function ($request, $handler) {
     $response = $handler->handle($request);
     return $response
         ->withHeader('Access-Control-Allow-Origin', 'http://localhost:5173')
+        ->withHeader('Access-Control-Allow-Credentials', 'true')
         ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 });
